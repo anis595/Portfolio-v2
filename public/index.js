7e8bd8b810e9c44c5ea2717ddd362be7d2235cd6
anis ghedabnia <Ghedabnia.anis@gmail.com>
2023-04-24 01:15:29 +0200

    add svg menu & js

---
+++ public/index.js
@@ -0,0 +1,6 @@
+// let btn1 = document.getElementById('btn1')
+// let menuNav = document.getElementById('menuNav')
+
+// btn1.addEventListener('click' , ()=> {
+//     menuNav.classList.toggle('hidden');
+// })
